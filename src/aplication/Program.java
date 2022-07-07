510d0e72a1dfea0acd8b5c7b913ec7829c366383
patricknasciment <patrick_evan@outlook.com>
2022-07-06 21:43:15 -0300

    findById implementation

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,10 +1,17 @@
 package aplication;
 
 import model.entities.Department;
+import model.entities.Seller;
+import model.entities.dao.DaoFactory;
+import model.entities.dao.SellerDao;
 
 public class Program {
 
     public static void main(String[] args) {
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
     }
 }
--- src/model/entities/dao/DaoFactory.java
+++ src/model/entities/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.entities.dao;
 
+import db.DB;
 import model.entities.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
     public static SellerDao createSellerDao(){
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 
 }
